c9f50c01b84b958fe1bdf0c99dc97187e5c0faa0
Nikolas Garofil <nikolas.garofil@uantwerpen.be>
2024-02-25 02:05:26 +0100

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Start the X server
 Xvfb :0 -screen 0 1024x768x24 -ac +extension GLX +render -noreset &
-# Make it accessible with VNC
-x11vnc -display :0 -forever -shared -nopw -quiet -bg -xkb
+# Make it accessible with VNC with password
+VNCPASS=$(pwgen -1 10)
+echo "Your VNC password is: $VNCPASS"
+x11vnc -storepasswd "$VNCPASS" ~/.vnc/passwd
+x11vnc -display :0 -forever -shared -quiet -bg -xkb -usepw
 # Start IPMIView in the X server
 DISPLAY=:0 /opt/IPMIView/IPMIView20 &
 # Make it accessible with http
