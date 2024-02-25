007967df4efa552a7d64a0ba074069f3c3e14a95
Nikolas Garofil <nikolas.garofil@uantwerpen.be>
2024-02-25 01:51:05 +0100

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# Start the X server
 Xvfb :0 -screen 0 1024x768x24 -ac +extension GLX +render -noreset &
+# Make it accessible with VNC
+x11vnc -display :0 -forever -shared -nopw -quiet -bg -xkb
+# Start IPMIView in the X server
 DISPLAY=:0 /opt/IPMIView/IPMIView20 &
-cd /usr/share/novnc/utils && ./launch.sh --vnc localhost:5900 &
-sleep infinity
+# Make it accessible with http
+cd /usr/share/novnc/utils && ./launch.sh --vnc localhost:5900
