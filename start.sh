4b0500b4b7879c36c85828c7b8e7038666ab06a4
Nikolas Garofil <nikolas.garofil@uantwerpen.be>
2024-02-25 02:43:22 +0100

    fix novnc not starting

--- start.sh
+++ start.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+
+# Starts a program on DISPLAY :0 and restarts it every time it exits
+keep_running_in_X() {
+    while true; do
+	DISPLAY=:0 "$1" &
+	wait $!	#$! is the PID of the last backgrounded process
+    done
+}
+
 # Start the X server
 Xvfb :0 -screen 0 1024x768x24 -ac +extension GLX +render -noreset &
+
 # Make it accessible with VNC with password
 VNCPASS=$(pwgen -1 10)
 echo "Your VNC password is: $VNCPASS"
-x11vnc -storepasswd "$VNCPASS" ~/.vnc/passwd
+mkdir -p ~/.vnc && x11vnc -storepasswd "$VNCPASS" ~/.vnc/passwd
 x11vnc -display :0 -forever -shared -quiet -bg -xkb -usepw
+
 # Start IPMIView in the X server
-DISPLAY=:0 /opt/IPMIView/IPMIView20 &
+keep_running_in_X /opt/IPMIView/IPMIView20 &
+
 # Make it accessible with http
 cd /usr/share/novnc/utils && ./launch.sh --vnc localhost:5900
